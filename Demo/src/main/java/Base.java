f7f57674291b52a89bc4afc74c70c65e535cf361
Deepak Kumar <deepak24121994@gmail.com>
2020-09-11 01:49:50 +0530

    invoking app
    with help of appium

---
+++ Demo/src/main/java/Base.java
@@ -0,0 +1,26 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base {
+    public static void main(String[] args) throws MalformedURLException {
+        File f= new File("src");
+        File fs=new File(f,"ApiDemos-debug.apk");
+
+        DesiredCapabilities dc=new DesiredCapabilities();
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME,"deepakemulator");
+        dc.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
+        dc.setCapability(MobileCapabilityType.AUTOMATION_NAME,"uiautomator2");
+        AndroidDriver<AndroidElement> driver
+                = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),dc);
+
+    }
+
+
+
+}
