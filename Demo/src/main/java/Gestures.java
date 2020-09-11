58f388f8770c9a3d6a135933c617b74185496e53
Deepak Kumar <deepak24121994@gmail.com>
2020-09-11 23:41:13 +0530

    long press

--- Demo/src/main/java/Gestures.java
+++ Demo/src/main/java/Gestures.java
@@ -1,18 +1,26 @@
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
 import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
+import java.time.Duration;
 
 public class Gestures extends Base {
     public static void main(String[] args) throws MalformedURLException {
         AndroidDriver driver = capabilities();
         driver.findElementByAndroidUIAutomator("text(\"Views\")").click();
-        TouchAction touchAction=new TouchAction(driver);
-        WebElement webElement= driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
+        TouchAction touchAction = new TouchAction(driver);
+        WebElement webElement = driver.findElementByXPath("//android.widget.TextView[@text='Expandable Lists']");
         touchAction.tap(TapOptions.tapOptions().withElement(ElementOption.element(webElement))).perform();
+        driver.findElementByAndroidUIAutomator("text(\"1. Custom Adapter\")").click();
+        WebElement webElement2 = driver.findElementByAndroidUIAutomator("text(\"People Names\")");
+        touchAction.longPress(LongPressOptions.longPressOptions()
+                .withElement(ElementOption.element(webElement2))
+                .withDuration(Duration.ofSeconds(2))).perform();
+        System.out.println(driver.findElementById("android:id/title").isDisplayed());
 
     }
 }
